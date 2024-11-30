e9d6e7c7b638345448467f88629aa9af994ca83a
esadakman <esadd26@gmail.com>
2024-11-30 13:34:56 +0300

    kinde installed for auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
-const Navbar = () => {
-  const user = undefined;
+const Navbar = async() => {
+  const {getUser} = getKindeServerSession();
+  const user = await getUser();
   const isAdmin = false;
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
